4c2fe580aa549f64722ca15d37c1065f62fd885f
Osman Jalloh <omudu45@hotmail.com>
2016-05-15 16:25:21 +0000

    implement the view

--- mudu/views.py
+++ mudu/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import About, Contact
+from .serializers import AboutSerializer, ContactSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def api(request):
+	""" List my Info, or Contact Me """
+	if request.method == 'GET':
+		about = About.objects.all()
+		serializer = AboutSerializer(about, many=True)
+		return Response(serializer.data)
+
+	elif request.method == 'POST':
+		serializer = ContactSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
