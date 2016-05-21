c4b1201d34bd450e3be4f8301913e8276945cc70
Osman Jalloh <omudu45@hotmail.com>
2016-05-21 14:26:20 +0000

    testing for SECRET_KEY

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -37,7 +37,7 @@ MANAGERS = ADMINS
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable("SECRET_KEY")
+SECRET_KEY = 'a#@!llid56$elgld-ldls!ldls(lldsl%lslshgnbldt&#)-f&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
