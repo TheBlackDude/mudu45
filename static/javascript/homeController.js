adde818c9b5e853dcb3db8320d38cfc828c6d118
Osman Jalloh <omudu45@hotmail.com>
2016-05-26 11:16:22 +0000

    delete the main static folder

--- static/javascript/homeController.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	'use strict';
-	angular.module('mudu.controllers')
-	.controller('HomeCtrl', ['$scope', function($scope){
-		var vm = this;
-		vm.text = 'Hi, I am MuDu45 or You Can Call Me TheBlackDude';
-	}]);
-
-})();
